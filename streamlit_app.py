639736ad4a466768cc3dae57f4dd6c3fd2be87c1
ricardotorrescazares <149017326+ricardotorrescazares@users.noreply.github.com>
2025-08-14 14:44:39 -0600

    Update streamlit_app.py
    Making Use of the SEARCH_ON Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,11 +86,12 @@ if ingredients_list: #Si la lista tiene valores, muestrala, si no, no
 
         #Statement That Will Get Us the "Search On" Value START
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         #Statement That Will Get Us the "Search On" Value END
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string) #The st.write() command should be part of the IF block but not part of the FOR Loop.
