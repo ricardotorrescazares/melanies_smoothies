4ca7ba52dbaea062c1bd424f97c7eeaa5a5a885d
ricardotorrescazares <149017326+ricardotorrescazares@users.noreply.github.com>
2025-08-14 14:29:49 -0600

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,10 +52,11 @@ session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) st.dataframe(data=my_dataframe, use_container_width=True) * This line use only selected columns
 
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop(
+st.stop()
 
 ingredients_list = st.multiselect(
     'Chose up to 5 ingredients:'
